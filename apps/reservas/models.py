e1612c307ea5b9f80578af3888ca82b94c9085bf
arielgonzalo <aibargonzalo0@gmail.com>
2025-10-14 12:39:42 -0300

    correcciones

--- apps/reservas/models.py
+++ apps/reservas/models.py
@@ -4,6 +4,5 @@ from django.db import models
 # Create your models here.
 
 class Reserva(models.Model):
-    usuario=models.ForeignKey('usuarios.Usuario', on_delete=models.CASCADE, related_name='reservas')
     recorrido=models.ForeignKey('recorridos.Recorrido', on_delete=models.CASCADE, related_name='reservas')
 
