8f4088333598337bec3f0a3a35ee98ca9c669362
arielgonzalo <aibargonzalo0@gmail.com>
2025-10-14 12:51:51 -0300

    correccion

--- citytur_catamarca/settings.py
+++ citytur_catamarca/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-AUTH_USER_MODEL = 'usuarios.Usuario' #defino mi usuario
+
 
 STATIC_URL = 'static/'
 
