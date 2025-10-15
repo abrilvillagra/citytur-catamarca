a738760dcfea1225865dfc4cdd5f94c99c19b817
arielgonzalo <aibargonzalo0@gmail.com>
2025-10-15 16:11:13 -0300

    se agregaron modelos al proyecto

--- apps/reservas/models.py
+++ apps/reservas/models.py
@@ -1,6 +1,24 @@
+from django.core.validators import MinValueValidator
 from django.db import models
 
 
 # Create your models here.
+class FormaPago(models.Model):
+    nombre=models.CharField(max_length=100)
+    descripcion=models.CharField(blank=True, null=True)
 
+    def __str__(self):
+        return f'{self.nombre}'
+
+class Reserva(models.Model):
+    forma_pago=models.ForeignKey(FormaPago, on_delete=models.CASCADE)
+    fechaCreacion=models.DateField(auto_now_add=True)
+    cant_personas=models.IntegerField(validators=[MinValueValidator(1)])
+    recorrido=models.ForeignKey('recorridos.Recorrido', on_delete=models.SET_NULL,
+                                   null=True,
+                                   related_name='reservas')
+    estado=models.BooleanField(default=True)
+
+    def __str__(self):
+        return f'reserva: {self.recorrido}, {self.estado}'
 
--- apps/unidades/models.py
+++ apps/unidades/models.py
@@ -1,4 +1,10 @@
 from django.db import models
-
+from django.core.validators import MinValueValidator
 # Create your models here.
 
+class Unidad(models.Model):
+    patente=models.CharField(max_length=100)
+    capacidad=models.IntegerField(validators=[MinValueValidator(1)])
+    marca=models.CharField(max_length=40)
+    modelo=models.CharField(max_length=40)
+    estado=models.BooleanField(default=True)
