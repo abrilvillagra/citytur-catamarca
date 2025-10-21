b99a81904e3a43c6c713b4737686457c94466b6e
arielgonzalo <aibargonzalo0@gmail.com>
2025-10-20 23:09:10 -0300

    modificaciones a gestion_recorridos

--- apps/reservas/forms.py
+++ apps/reservas/forms.py
@@ -1,5 +1,10 @@
 from django import forms
+from django.forms.widgets import TextInput
+
 from .models import Recorrido, Parada
+from django import forms
+
+from apps.reservas.models import Recorrido
 
 class RecorridoForm(forms.ModelForm):
     paradas=forms.ModelMultipleChoiceField(
@@ -14,4 +19,9 @@ class RecorridoForm(forms.ModelForm):
     class Meta:
         model=Recorrido
         fields=['nombre', 'precio', 'hora_salida', 'hora_llegada', 'estado', 'descripcion', 'paradas']
+        widgets={
+            'nombre': forms.TextInput(attrs={'required':'True'}),
+            'precio': forms.NumberInput(attrs={'min':'1', 'required':'True'}),
+            'hora_salida': forms.TimeInput(attrs={})
 
+        }
