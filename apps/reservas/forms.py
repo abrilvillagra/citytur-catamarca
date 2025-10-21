a32a75748125b55bfe9c51527e6be6118edcb03e
arielgonzalo <aibargonzalo0@gmail.com>
2025-10-21 00:08:16 -0300

    se termino de editar el tamplate

--- apps/reservas/forms.py
+++ apps/reservas/forms.py
@@ -1,17 +1,15 @@
-from django import forms
-from django.forms.widgets import TextInput
 
-from .models import Recorrido, Parada
 from django import forms
 
-from apps.reservas.models import Recorrido
+from apps.reservas.models import Recorrido, Parada
 
 class RecorridoForm(forms.ModelForm):
     paradas=forms.ModelMultipleChoiceField(
         queryset=Parada.objects.filter(estado=True),
         widget=forms.SelectMultiple(attrs={
             'class':'form-select',
-            'id':'paradas'
+            'id':'paradas',
+            'size': 5
         }),
         required=False
     )
@@ -19,9 +17,15 @@ class RecorridoForm(forms.ModelForm):
     class Meta:
         model=Recorrido
         fields=['nombre', 'precio', 'hora_salida', 'hora_llegada', 'estado', 'descripcion', 'paradas']
+        ESTADOS = [
+            (True, 'Activo'),
+            (False, 'Inactivo'),
+        ]
         widgets={
             'nombre': forms.TextInput(attrs={'required':'True'}),
             'precio': forms.NumberInput(attrs={'min':'1', 'required':'True'}),
-            'hora_salida': forms.TimeInput(attrs={})
-
+            'hora_salida': forms.TimeInput(attrs={'placeholder': 'Ej: 10:30'}),
+            'hora_llegada': forms.TimeInput(attrs={'placeholder': 'Ej: 10:30'}),
+            'estado' : forms.Select(choices=ESTADOS, attrs={'class':'form-select', 'aria-label':'Default select example'}),
+            'descripcion': forms.Textarea(attrs={'rows':'3', 'cols':'40'})
         }
