b65e948aea832c1d87ea9abf6b158aa067f31d43
arielgonzalo <aibargonzalo0@gmail.com>
2025-10-23 19:24:39 -0300

    se agrego un admin

--- apps/reservas/admin.py
+++ apps/reservas/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import PuntoTuristico, Recorrido
 
 # Register your models here.
+
+@admin.register(PuntoTuristico)
+class PuntoTuristicoAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'categoria', 'descripcion' ,'ubicacion', 'estado')
+
+@admin.register(Recorrido)
+class RecorridoAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'hora_salida', 'estado', 'precio')
