498d0e8565d1e626a3b30064624f79d0508f14a4
arielgonzalo <aibargonzalo0@gmail.com>
2025-10-23 18:46:38 -0300

    se modifico gestion_recorridos.html

--- apps/reservas/forms.py
+++ apps/reservas/forms.py
@@ -3,30 +3,42 @@ from django import forms
 
 from apps.reservas.models import Recorrido, PuntoTuristico
 
+from django.core.exceptions import ValidationError
+
 class RecorridoForm(forms.ModelForm):
-    # paradas=forms.ModelMultipleChoiceField(
-    #     queryset=PuntoTuristico.objects.filter(estado=True),
-    #     widget=forms.SelectMultiple(attrs={
-    #         'class':'form-select',
-    #         'id':'paradas',
-    #         'size': 5
-    #     }),
-    #     required=False
-    # )
+
+    ESTADOS = [
+        (True, 'Activo'),
+        (False, 'Inactivo'),
+    ]
+    estado=forms.TypedChoiceField(choices=ESTADOS,
+                                  coerce=lambda val: val=='True',
+                                  widget=forms.Select(attrs={'class':'form-select','aria-label':'Default select example'}),
+                                  required=False)
+
 
     class Meta:
         model=Recorrido
         fields=['nombre', 'precio', 'hora_salida', 'hora_llegada', 'estado', 'descripcion', 'imagen', 'puntos_turisticos']
-        ESTADOS = [
-            (True, 'Activo'),
-            (False, 'Inactivo'),
-        ]
+
         widgets={
-            'nombre': forms.TextInput(attrs={'required':'True'}),
-            'precio': forms.NumberInput(attrs={'min':'1', 'required':'True'}),
-            'hora_salida': forms.TimeInput(attrs={'placeholder': 'Ej: 10:30'}),
-            'hora_llegada': forms.TimeInput(attrs={'placeholder': 'Ej: 10:30'}),
-            'estado' : forms.Select(choices=ESTADOS, attrs={'class':'form-select', 'aria-label':'Default select example'}),
+            'nombre': forms.TextInput(attrs={'required':'required'}),
+            'precio': forms.NumberInput(attrs={'min':'1', 'required':'required'}),
+            'hora_salida': forms.TimeInput(format='%H:%M', attrs={'placeholder': 'Ej: 10:30', 'type': 'time'}),
+            'hora_llegada': forms.TimeInput(format='%H:%M',attrs={'placeholder': 'Ej: 10:30', 'type': 'time'}),
             'descripcion': forms.Textarea(attrs={'rows':'3', 'cols':'40'}),
-            'imagen': forms.ClearableFileInput()
+            'imagen': forms.ClearableFileInput(attrs={'class':'form-select'}),
+            'puntos_turisticos': forms.SelectMultiple(attrs={'class':'form-select', 'size':'5'})
         }
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['puntos_turisticos'].queryset = PuntoTuristico.objects.filter(estado=True)
+
+    def clean(self):
+        cleaned_data=super().clean()
+        hora_salida=cleaned_data.get('hora_salida')
+        hora_llegada=cleaned_data.get('hora_llegada')
+
+        if hora_salida and hora_llegada and hora_salida >= hora_llegada:
+            raise ValidationError("La hora de llegada debe ser posterior a la hora de salida")
+        return cleaned_data
--- apps/reservas/views.py
+++ apps/reservas/views.py
@@ -9,11 +9,13 @@ import logging
 # Create your views here.
 
 def agregar_recorrido(request):
-
+    nuevo_recorrido=None
     if request.method=='POST':
-        recorrido_form=RecorridoForm(request.POST)
+        recorrido_form=RecorridoForm(request.POST, request.FILES)
         if recorrido_form.is_valid():
-            recorrido_form.save()
+            nuevo_recorrido=recorrido_form.save(commit=False)
+            nuevo_recorrido.save()
+            recorrido_form.save_m2m()
             messages.success(request, "Recorrido guardado correctamente.")
             return redirect('reservas:agregar_recorrido')
         else:
